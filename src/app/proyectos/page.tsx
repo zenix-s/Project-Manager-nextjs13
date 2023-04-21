f176aceb8ac50536ed7fd596b463acbd7e6d1581
shadowborne <sergiofs19@gmail.com>
2023-04-21 17:24:50 +0200

    Update page.tsx

--- src/app/proyectos/page.tsx
+++ src/app/proyectos/page.tsx
@@ -2,9 +2,6 @@ import getProyectos from "../../actions/getProyectos";
 import CardProyecto from "../../components/proyectos/cardproyecto";
 import NewProjectForm from "../../components/proyectos/formularionuevoproyecto";
 
-
-
-
 const Page = async () => {
   const proyectos = await getProyectos();
 
@@ -12,16 +9,16 @@ const Page = async () => {
     <section
       className="
       flex
-      flex-col
       h-full
-      overflow-hidden
       w-full
+      flex-col
+      overflow-hidden
     "
     >
       <div className="">
         <NewProjectForm visible={false} />
       </div>
-      <div className="w-full overflow-y-scroll p-2 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+      <div className="grid w-full grid-cols-1 gap-4 overflow-y-scroll p-2 md:grid-cols-2 xl:grid-cols-3">
         {proyectos.map((proyecto) => (
           <CardProyecto
             key={proyecto.id}
