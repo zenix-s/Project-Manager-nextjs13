452602f41f6f362ff08ae617c32217dc3be5f9f7
Zen_Shadowborne <sergiofs19@gmail.com>
2023-04-26 17:06:11 +0200

    Update modalcontainer.tsx

--- src/components/modals/modalcontainer.tsx
+++ src/components/modals/modalcontainer.tsx
@@ -1,6 +1,6 @@
 import Button from "../button";
 import { useState } from "react";
-import {CiCircleRemove} from 'react-icons/ci'
+import { CiCircleRemove } from "react-icons/ci";
 interface FormContainerProps {
   title: string;
   body: React.ReactElement;
@@ -21,11 +21,13 @@ const ModalContainer: React.FC<FormContainerProps> = ({
   onClose,
 }) => {
   return (
-    <section className={`absolute top-0 left-0 z-[100] bg-neutral-500/30 
+    <section
+      className={`absolute left-0 top-0 z-[100] bg-neutral-500/30 
     ${visible ? "flex" : "hidden"}
-    h-full w-full items-center justify-center`}>
+    h-full w-full items-center justify-center`}
+    >
       <div className="relative h-full w-full rounded-lg bg-slate-400 p-4 md:h-fit md:w-1/2 ">
-        <div className="absolute top-0 right-0 p-4">
+        <div className="absolute right-0 top-0 p-4">
           <Button
             label=""
             icon={CiCircleRemove}
@@ -38,7 +40,7 @@ const ModalContainer: React.FC<FormContainerProps> = ({
             trasparent
           />
         </div>
-        <div className=" mb-4 h-16 w-full border-b-2 border-slate-200/30 ">
+        <div className=" mb-4  h-16 w-full  border-b-2 border-slate-200/30  ">
           <h2 className=" p-4 text-center text-2xl uppercase tracking-widest text-white ">
             {title}
           </h2>
@@ -49,7 +51,14 @@ const ModalContainer: React.FC<FormContainerProps> = ({
             className=" flex h-full w-full flex-col justify-center "
           >
             {body}
-            <Button label={actionlabel} theme="dark" onClick={() => {}} center shadow uppercase />
+            <Button
+              label={actionlabel}
+              theme="dark"
+              onClick={() => {}}
+              center
+              shadow
+              uppercase
+            />
           </form>
         </div>
         {footer && <div className="form-footer">{footer}</div>}
