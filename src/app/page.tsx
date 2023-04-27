0a3b1bfb33b1fda2b25aeb4a0b69f60e50a744ea
shadowborne <sergiofs19@gmail.com>
2023-04-27 11:38:01 +0200

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
-import Header from "../components/header/header"
+import Header from "../components/header/header";
 import getCurrentUser from "@/actions/getCurrentUser";
- 
-
 
 const Page = async () => {
   const user = await getCurrentUser();
   const id = user ? user.id : null;
 
-
   return (
-    <section>
-      <h1>Page</h1>
-      <p>Main</p>
-      { id ? <p>Logged id {id}</p> : <></>}
+    <section className="w-full h-full">
+      <div className="w-full h-full flex items-center justify-center text-[15vw] font-extrabold">
+        <h1>VARBAS</h1>
+      </div>
+      {id ? <p>Logged id {id}</p> : <></>}
     </section>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/components/kanban/cardTarea.tsx
+++ src/components/kanban/cardTarea.tsx
@@ -13,8 +13,9 @@ const CardTarea = ({ tarea }: { tarea: TareaProps }) => {
     <div className="bg-white rounded-md p-4 shadow cursor-pointer select-none">
       <h3 className="font-bold">{tarea.nombre}</h3>
       <p className="text-gray-600 mb-2">{tarea.description}</p>
-      <p className="text-gray-600 text-right">{
-      tarea.endDate.toLocaleDateString()
+      <p className="text-gray-600 text-right">
+        {
+        tarea.endDate.toLocaleDateString()
       }</p>
     </div>
   );
