ad4dcc5d2e919ab8fb52e4c0bd3ea41f36708ff2
shadowborne <sergiofs19@gmail.com>
2023-05-02 21:17:22 +0200

    Update cardproyecto.tsx

--- src/components/proyectos/cardproyecto.tsx
+++ src/components/proyectos/cardproyecto.tsx
@@ -3,6 +3,8 @@ import { FiMenu, FiExternalLink } from "react-icons/fi";
 import { useState } from "react";
 import Button from "../button";
 import { useRouter } from "next/navigation";
+import loadingLogo from "../../assets/svg/loading.svg";
+import Image from "next/image";
 
 interface CardProyectoProps {
   name: String;
@@ -20,6 +22,7 @@ const CardProyecto = ({
   idProyecto,
 }: CardProyectoProps) => {
   const [visible, setVisible] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
 
   const toogleVisible = () => {
     setVisible(!visible);
@@ -32,15 +35,16 @@ const CardProyecto = ({
   };
 
   const deleteProject = async () => {
+    setDeleting(true);
     const IdProyecto = idProyecto as string;
     try {
       const response = await fetch("http://localhost:3000/api/proyectos", {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-          'id-proyecto': IdProyecto,
-        }
+          "id-proyecto": IdProyecto,
+        },
       });
-    
+
       if (response.ok) {
         const result = await response.json();
         console.log(result);
@@ -53,14 +57,25 @@ const CardProyecto = ({
 
   return (
     <div className="relative m-2 flex h-52 cursor-pointer flex-col items-start justify-between overflow-hidden rounded-xl bg-white p-4 shadow-md">
+      <div
+        className={`
+      ${isDeleting ? "flex" : "hidden"} 
+      text-dark absolute left-0 top-0 h-full w-full items-center  justify-center bg-white`}
+      >
+        <Image src={loadingLogo} alt="Loading..." width={100} height={100} />
+      </div>
+
       <button
-        className=" absolute right-4 top-4 z-10"
+        className={`
+          ${isDeleting ? "hidden" : "flex"}
+          absolute right-4 top-4 z-10 
+          `}
         onClick={() => toogleVisible()}
       >
         <FiMenu size={24} />
       </button>
 
-      <div className="absolute bottom-0 right-0">
+      <div className={`${isDeleting? "hidden" : "flex"} absolute bottom-0 right-0`}>
         <Button
           label=""
           theme="transparent"
@@ -74,6 +89,7 @@ const CardProyecto = ({
       <div
         className={`
       ${visible ? "flex" : "hidden"}
+      ${isDeleting ? "hidden" : "flex"}
       absolute
       left-0
       top-0
@@ -94,7 +110,6 @@ const CardProyecto = ({
           textColor="black"
           center
           shadow
-
         />
         <Button
           label="Eliminar"
