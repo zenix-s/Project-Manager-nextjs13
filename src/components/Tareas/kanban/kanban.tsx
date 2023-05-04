cb861e408afcf01fc8a8d452a6498ba24dfeb88b
Zen_Shadowborne <sergiofs19@gmail.com>
2023-05-04 10:28:18 +0200

    scroll kanban solved

--- src/components/Tareas/kanban/kanban.tsx
+++ src/components/Tareas/kanban/kanban.tsx
@@ -10,8 +10,8 @@ const Kanban = ({
   estados: EstadoProps[];
 }) => {
   return (
-    <div className="flex h-full w-full flex-row pt-4">
-      <div className="flex flex-row gap-4 overflow-x-scroll">
+    <div className="flex h-full w-full flex-row relative">
+      <div className="flex py-3 flex-row gap-4 overflow-x-auto overflow-y-hidden absolute top-0 left-0 bottom-0 right-0">
         {estados.map((estado: EstadoProps) => {
           return (
             <ColEstado
