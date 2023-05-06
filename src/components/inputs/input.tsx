87b3a3f4b98745712b693e95a3618b17d93c8485
shadowborne <sergiofs19@gmail.com>
2023-05-06 02:19:00 +0200

    Update input.tsx

--- src/components/inputs/input.tsx
+++ src/components/inputs/input.tsx
@@ -17,11 +17,11 @@ const Input: React.FC<InputProps> = ({
         placeholder=" "
         {...register(id, { required })}
         id={id}
-        className="focus:shadow-outline peer h-10 w-full rounded-lg border px-2 text-gray-700 placeholder-gray-400"
+        className="focus:shadow-outline text-lg peer w-full rounded-lg border px-2 py-3 text-gray-700 placeholder-gray-400"
       />
       <label
         htmlFor={id}
-        className="absolute -top-5 left-2 cursor-text text-gray-200 transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-gray-500 peer-focus:-top-5 peer-focus:text-gray-200"
+        className="absolute -top-6 text-lg left-2 cursor-text text-gray-200 transition-all peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-gray-500 peer-focus:-top-6 peer-focus:text-gray-200"
       >
         {label}{" "}
         {errors[id] && (
